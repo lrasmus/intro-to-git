c4fc460961adf11c4b94d5b7eca361b6e957146d
Luke Rasmussen <luke.rasmussen@gmail.com>
2018-05-16 14:31:03 -0500

    Initial add of files

---
+++ code/analysis.r
@@ -0,0 +1,12 @@
+# I'm doing some testing
+
+# Here's some work I don't need right now.
+
+# We're just pretending this is a really cool analysis.  The whole purpose
+# of this is to have some code that we can put under source control.
+
+setwd('/Users/lvr491/Development/Training/intro-to-git/')
+data <- read.csv("./data/input/magic-data.csv")
+write.csv(data, file="./data/output/magic-output.csv")
+
+# Down at the bottom
